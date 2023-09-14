6c7807b776b303815a1a43c5b58bdb4b46afab68
Sonjuan <simset1212@gmail.com>
2023-09-14 12:18:33 +0900

    revisit 2178

--- bfs/2178-미로탐색/2178.cpp
+++ bfs/2178-미로탐색/2178.cpp
@@ -1,25 +1,52 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
 #include <string>
+
 using namespace std;
-#define X first
-#define Y second
-string board[102];
+
+int N;
+int M;
+vector<string> table(102);
+
+int dr[4] = {-1, 1, 0, 0};
+int dc[4] = {0, 0, 1, -1};
 int dist[102][102];
-int n, m;
-int dx[4] = {0, 0, 1, -1};
-int dy[4] = {1, -1,0, 0};
+
+int bfs() {
+    queue<pair<int,int>> q;
+    q.push({0,0});
+    for(int i = 0; i < N; ++i) {
+        fill(dist[i], dist[i]+M, -1);
+    }
+    dist[0][0] = 1;
+    while(!q.empty()) {
+        auto [r, c] = q.front(); q.pop();
+
+        for(int i = 0; i < 4; ++i) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+
+            if(nr < 0 || nc < 0 || nr >= N || nc >= M) continue;
+            if(table[nr][nc] == '0') continue;
+            if(dist[nr][nc] != -1) continue;
+
+            dist[nr][nc] = dist[r][c] + 1;
+            q.push({nr, nc});
+        }
+    }
+
+    return dist[N-1][M-1];
+}
+
 
 int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> n >> m;
-    for(int i = 0; i <n; ++i) {
-        cin >> board[i];
+    cin >> N >> M;  
+    for(int i = 0; i < N; ++i) {
+        cin >> table[i];
     }
-    for(int i = 0; i < n; ++i)  fill(dist[i], dist[i]+m, -1);
 
+    cout << bfs();
 
     return 0;
 }
