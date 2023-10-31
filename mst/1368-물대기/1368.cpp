84c3a8ca1620f16eac39477f43b29908abd22f96
Sonjuan <simset1212@gmail.com>
2023-10-31 22:51:49 +0900

    solved 1368

---
+++ mst/1368-물대기/1368.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+using namespace std;
+
+int N;
+vector<int> link(305, -1);
+vector<tuple<int,int,int>> edge;
+
+int find(int x) {
+    if(link[x] < 0) return x;
+    return link[x] = find(link[x]);
+}
+
+bool is_diff_group(int u, int v) {
+    u = find(u); v = find(v);
+    if(u == v) return 0; // same parent
+    if(link[u] == link[v]) link[u] -= 1;
+    if(link[u] < link[v]) link[v] = u;
+    else link[u] = v;
+    return 1;
+}
+
+
+int main(void) {
+    cin >> N;
+    for(int i = 1; i <= N; ++i) {
+        int w;
+        cin >> w;
+        edge.push_back({w, 0, i});
+    }
+
+    for(int i = 1; i <= N; ++i) {
+        for(int j = 1; j <= N; ++j) {
+            int w;
+            cin >> w;
+            if(i >= j) continue;
+            edge.push_back({w, i, j});
+        }
+    }
+    
+    std::sort(edge.begin(), edge.end());
+    int ans = 0;
+    int cnt = 0;
+    for(int i = 0; i < edge.size(); ++i) {
+        auto [w, a, b] = edge[i];
+        if(!is_diff_group(a, b)) continue;
+        ans += w;
+        cnt += 1;
+        if(cnt == N) break;
+    }
+    
+    cout << ans;
+
+    return 0;
+}
