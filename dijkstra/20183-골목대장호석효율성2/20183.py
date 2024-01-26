a4bbac4983410f84bf3a3003360be3604674963b
Sonjuan <simset1212@gmail.com>
2024-01-26 10:59:21 +0900

    solved 20183

--- dijkstra/20183-골목대장호석효율성2/20183.py
+++ dijkstra/20183-골목대장호석효율성2/20183.py
@@ -5,34 +5,46 @@ input = sys.stdin.readline
 
 N, M, A, B, C = map(int, input().split())
 graph = defaultdict(list)
+edges = set()
 
 for _ in range(M) :
     u,v,c = map(int, input().split())
     graph[u].append((v,c))
     graph[v].append((u,c))
+    edges.add(c)
 
-result = {i : float('inf') for i in range(1,N+1)}
-result[A] = 0
-ans = float('inf')
-pq = [(0, A, 0)]
+def path_finding(target_weight) :
+    pq = [(0,A)]
+    result = {i : float('inf') for i in range(1,N+1)}
+    result[A] = 0
 
-while pq :
-    w, cur, mw = heappop(pq)
-    if cur == B :
-        if w <= C :
-            ans = min(ans,mw)
-            
-        continue
+    while pq :
+        w, cur = heappop(pq)
     
-    if result[cur] < w :
-        continue
+        if result[cur] < w :
+            continue
 
-    for nxt, nw in graph[cur] :
-        if w + nw < result[nxt] :
-            result[nxt] = w + nw
-            heappush(pq, (w+nw, nxt,max(mw,nw)))
+        for nxt, nw in graph[cur] :
+            if nw <= target_weight and w+nw < result[nxt] :
+                result[nxt] = w+nw
+                heappush(pq, (w+nw, nxt))
 
-if ans != float('inf'):
-    print(ans)
-else :
-    print(-1)
+    if result[B] <= C :
+        return result[B]
+    else :
+        return -1
+
+left = 0
+right = len(edges) -1
+edges = sorted(list(edges))
+ans = -1
+
+while left <= right :
+    mid = (left - right)//2 + right
+    if path_finding(edges[mid]) != -1 :
+        right = mid - 1
+        ans = edges[mid] 
+    else :
+        left = mid + 1
+
+print(ans)
