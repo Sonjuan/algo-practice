e63d87ccdb8d283a6904b3bec3a9442b0c8197ff
Sonjuan <simset1212@gmail.com>
2024-06-24 14:34:31 +0900

    solved 1162

--- dijkstra/1162-도로포장/1162.py
+++ dijkstra/1162-도로포장/1162.py
@@ -1,20 +1,45 @@
-
-
-
 from heapq import heappush, heappop
 from collections import defaultdict
+import sys
 
-N, M, K = map(int, input().split())
+N, M, K = map(int, sys.stdin.readline().split())
 
-graph = defaultdict(list)
+graph = [[] for _ in range(N+1)]
 
 for _ in range(M) :
-    u,v,c = map(int, input().split())
+    u,v,c = map(int, sys.stdin.readline().split())
     graph[u].append((v,c))
     graph[v].append((u,c))
 
+dist = [[float('inf')] * (K + 1) for _ in range(N+1)]
+pq = []
 
+for i in range(K+1) :
+    dist[1][i] = 0
 
+heappush(pq, (0,1,0))
+
+while pq :
+    cur_dist, node, k = heappop(pq)
+    # eliminate arranged value
+    if dist[node][k] < cur_dist :
+        continue
+
+    if k + 1 <= K :
+        for (nxt, nxt_dist) in graph[node] :
+            if dist[nxt][k+1] > cur_dist :
+                dist[nxt][k+1] = cur_dist
+                heappush(pq, (cur_dist, nxt, k+1))
+    
+    for (nxt, nxt_dist) in graph[node] :
+        if dist[nxt][k] > cur_dist + nxt_dist :
+            dist[nxt][k] = cur_dist +nxt_dist
+            heappush(pq, (cur_dist+nxt_dist, nxt, k))
+
+ans = float('inf')
+for i in range(K+1) :
+    ans = min(ans, dist[N][i])
+print(ans)
 
 
 # 1<=N<=10,000
